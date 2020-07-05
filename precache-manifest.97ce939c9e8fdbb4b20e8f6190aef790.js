33da28e24a15147b41764f3ff1decaf7e247f61b
Isgramor9993999 <maksaj9@gmail.com>
2020-07-06 02:53:32 +0400

    FC

---
+++ precache-manifest.97ce939c9e8fdbb4b20e8f6190aef790.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08d5a8713b54f26ffcc168526df4b11",
+    "url": "/skymp/index.html"
+  },
+  {
+    "revision": "e241e982e9b46532a606",
+    "url": "/skymp/static/css/main.228f3b60.chunk.css"
+  },
+  {
+    "revision": "a3d239f560ed6db4b97e",
+    "url": "/skymp/static/js/2.d63b154d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skymp/static/js/2.d63b154d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e241e982e9b46532a606",
+    "url": "/skymp/static/js/main.3f3dd45a.chunk.js"
+  },
+  {
+    "revision": "7bc1d69a6ec301e5df29",
+    "url": "/skymp/static/js/runtime-main.f81902da.js"
+  },
+  {
+    "revision": "9e14b4e72dec1db9a60d2636bbfe64f2",
+    "url": "/skymp/static/media/San-Francisco.9e14b4e7.ttf"
+  },
+  {
+    "revision": "261b9e882421909782837b314c26cb7e",
+    "url": "/skymp/static/media/bac.261b9e88.jpg"
+  }
+]);
